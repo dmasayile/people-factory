53eccc336e796b619614cf91911c4e4a93cd911c
Davillious Masayile <user@users-mbp.coldspring.marian.edu>
2017-06-06 21:00:30 -0400

    Append the name as a proper DOM element

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function handleSubmit(ev) {
     const g = ev.target
     const details = document.querySelector('#details')
     const name = f.personName.value
+
+    const boldedName = document.createElement('strong')
+    boldedName.textContent = name
     
-    details.innerHTML += name
+    details.appendChild(boldedName)  
     
     // heading.textContent = f.personName.value + ' is approximately ' 
     // + g.personAge.value + ' years old. That sucks. One year closer to death.'
