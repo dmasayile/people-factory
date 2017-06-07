c108b6e04f705e3410411025aa83f0c8635d8e77
Davillious Masayile <user@users-mbp.coldspring.marian.edu>
2017-06-06 20:48:28 -0400

    Add name to details

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const g = ev.target
-    const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value + ' is approximately ' 
-    + g.personAge.value + ' years old. That sucks. One year closer to death.'
+    const details = document.querySelector('#details')
+    const name = f.personName.value
+    
+    details.innerHTML += name
+    
+    // heading.textContent = f.personName.value + ' is approximately ' 
+    // + g.personAge.value + ' years old. That sucks. One year closer to death.'
 }
 
 personForm.addEventListener('submit', handleSubmit)
