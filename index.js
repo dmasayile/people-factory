5a0e6943d11bcde7f3022b5a27c60cc59714924b
Davillious Masayile <user@users-mbp.coldspring.marian.edu>
2017-06-06 20:21:47 -0400

    Add second input and change heading (Day 1 HW)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const personForm = document.querySelector('#personForm')
+const personAge = document.querySelector('#personAge')
 
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
+    const g = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.personName.value + ' is approximately ' 
+    + g.personAge.value + ' years old. That sucks. One year closer to death.'
 }
 
 personForm.addEventListener('submit', handleSubmit)
